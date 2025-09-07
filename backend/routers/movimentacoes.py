0709a31eb0cdc74721ba587728190c5fdd1f0fce
Kayke <kayke.gsousa15@gmail.com>
2025-09-07 19:49:44 -0300

    API de consulta - Tabela Movimentacoes
    API de consulta de dados na tabela MOVIMENTACOES, referente as movimentações de produtos

---
+++ backend/routers/movimentacoes.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, HTTPException
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from config import load_config
+
+router = APIRouter()
+
+def create_db_session(env_type: str):
+    DATABASE_URL, ENV_TYPE = load_config(env_type)
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(bind=engine)
+    return SessionLocal, ENV_TYPE
+
+@router.get("/movimentacoes")
+def listar_movimentacoes():
+    SessionLocal, ENV_TYPE = create_db_session("leitura")
+    db = SessionLocal()
+    try:
+        result = db.execute(text("SELECT * FROM movimentacoes ORDER BY id")).fetchall() 
+        return [
+            {
+                "id": row.id,
+                "produto_id": row.produto_id, 
+                "quantidade": row.quantidade, 
+                "data": row.data,
+                "tipo_mov_id": row.tipo_mov_id, 
+                "preco_venda": row.preco_venda,
+                "preco_compra": row.preco_compra,  
+                "fornecedor_id": row.fornecedor_id, 
+                "tipo_pag_id": row.tipo_pag_id
+            } for row in result
+        ]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar categorias: {str(e)}")
+    finally:
+        db.close()
