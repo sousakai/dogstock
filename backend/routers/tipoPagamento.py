6adc66a3cc910f80e6efcd5558bb21d27a2bd96e
Kayke <kayke.gsousa15@gmail.com>
2025-09-07 20:22:38 -0300

    API de consulta - Tabela Tipo_Pagamento
    API para consulta de dados na tabela TIPO_PAGAMENTO, sendo esta uma tabela de referência para o tipo de pagamento na tabela MOVIMENTACOES

---
+++ backend/routers/tipoPagamento.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from config import load_config
+
+router = APIRouter()
+
+def create_db_session(env_type: str):
+    DATABASE_URL, ENV_TYPE = load_config(env_type)
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(bind=engine)
+    return SessionLocal, ENV_TYPE
+
+@router.get("/tipopagamento")
+
+
+def listar_pagamentos():
+    SessionLocal, ENV_TYPE = create_db_session("leitura")
+    db = SessionLocal()
+    try:
+        result = db.execute(text("SELECT * FROM tipo_pagamento ORDER BY id")).fetchall()
+        return [{"id": row.id, #o nome definido nas aspas é o nome final que vai ser encontrado pelo JS, independentemente do nome da tabela.
+                 "descricao": row.descricao
+                 } for row in result]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar tipos de pagamento: {str(e)}")
+    finally:
+        db.close()
+
